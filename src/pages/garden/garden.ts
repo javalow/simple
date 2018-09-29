3f0ed6f4b2f7ee5e538ab9aeb700290ab3796f93
javalow <jeremy@avalow.com>
2018-09-28 20:34:36 -0700

    added firebase test on garden

--- src/pages/garden/garden.ts
+++ src/pages/garden/garden.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { Intercom } from '@ionic-native/intercom';
+import { AuthService } from '../../providers/auth/auth';
+import * as firebase from 'firebase';
 
 
 @Component({
@@ -8,15 +10,27 @@ import { Intercom } from '@ionic-native/intercom';
   templateUrl: 'garden.html',
 })
 export class GardenPage {
+  public items: any [] = [];
+  public itemRef: firebase.database.Reference = firebase.database().ref('/ftproducts');
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private intercom: Intercom) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private intercom: Intercom)
+    {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad GardenPage');
-
     this.intercom.hideMessenger();
-  }
+    this.itemRef.on('value', itemSnapshot => {
+    this.items = [];
+    itemSnapshot.forEach( itemSnap => {
+      this.items.push(itemSnap.val());
+      return false;
+    });
+    })
+    }
 
 
 }
